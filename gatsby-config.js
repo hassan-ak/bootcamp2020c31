7276d6c86f15efd80c84f9e071f02e5baa5dba6e
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-06 05:08:58 +0500

    Step06- Create Posts on Contentful and test in graphql explorer
    1:- Create post on contentful
    	a:- Signup to Contentful
    	b:- create an organization
    	c:- create a space
    	d:- From content model add content Type
    	e:- add fields
    	f:- create content
    	g:- create keys
    2:- Installed
    	a:- contentful source plugin
    		i:- "yarn add gatsby-source-contentful"
    	b:- dotenv
    		i:- "yarn add dotenv"
    3:- Created
    	a:- .env
    4:- Updated
    	a:- gatsby-config.js
    	b:- .gitignore
    5:- Fetch data from contentful using graphql explorer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,17 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 module.exports = {
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-material-ui`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-material-ui`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 };
