7e947815d9327aa51bfe422645241756aaef6b73
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-07 02:19:53 +0500

    Step11:- Update Error Page
    1:- Updated
    	a:- src/pages/404.tsx
    2:- Created
    	a:- src/components/error/ErrorPage.tsx
    	b:- src/components/error/ErrorPage.css

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import Footer from "../components/addOns/Footer";
+import Header from "../components/addOns/Header";
+import { ErrorPage } from "../components/error/ErrorPage";
 
 export default function Error() {
-  return <div>Page not Found</div>;
+  return (
+    <div>
+      <Header />
+      <ErrorPage />
+      <Footer />
+    </div>
+  );
 }
