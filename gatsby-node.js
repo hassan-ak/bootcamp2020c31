0ac1706b906b4f42843c98e9179734531393d8c9
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-07 22:06:19 +0500

    Step12:- Update Pages
    1:- Created
    	a:- src/templates/blog-post.css
    2:- Updated
    	a:- gatsby-node.js
    	a:- src/components/blogs/BlogContent.tsx

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,22 +7,6 @@ exports.createPages = async ({ graphql, actions }) => {
       allContentfulBlogPost {
         edges {
           node {
-            title
-            summary
-            body {
-              raw
-            }
-            free
-            image {
-              gatsbyImageData(
-                width: 400
-                height: 200
-                layout: CONSTRAINED
-                placeholder: TRACED_SVG
-              )
-            }
-            source
-            publishedDate
             slug
           }
         }
@@ -31,9 +15,11 @@ exports.createPages = async ({ graphql, actions }) => {
   `);
   result.data.allContentfulBlogPost.edges.forEach((edge) => {
     createPage({
-      path: `/blogs/${edge.node.slug}`,
+      path: `blogs/${edge.node.slug}`,
       component: path.resolve("./src/templates/blog-post.tsx"),
-      context: edge.node,
+      context: {
+        slug: edge.node.slug,
+      },
     });
   });
 };
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,15 +1,68 @@
 import React from "react";
-import { graphql, useStaticQuery } from "gatsby";
+import { graphql } from "gatsby";
+import "./blogPost.css";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import "./blogPost.css";
+import Header from "../components/addOns/Header";
+import Footer from "../components/addOns/Footer";
 
-export default ({ pageContext }) => {
+export const query = graphql`
+  query($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      body {
+        raw
+      }
+      image {
+        gatsbyImageData(
+          width: 400
+          height: 200
+          layout: CONSTRAINED
+          placeholder: TRACED_SVG
+        )
+      }
+      source
+      publishedDate(formatString: "DD MMMM YYYY")
+    }
+  }
+`;
+
+const BlogPost = (props: any) => {
+  console.log(props.data.contentfulBlogPost);
   return (
     <div>
-      <div>
-        <h3>Title</h3>
-        <p>{pageContext.title}</p>
+      <Header />
+      <div className='blogPostDiv'>
+        <div>
+          <h1>{props.data.contentfulBlogPost.title}</h1>
+        </div>
+        <div className='iamgeDiv'>
+          <GatsbyImage
+            image={getImage(props.data.contentfulBlogPost.image)}
+            alt={props.data.contentfulBlogPost.title}
+            className='image'
+          />
+        </div>
+        <div className='content'>
+          {documentToReactComponents(
+            JSON.parse(props.data.contentfulBlogPost.body.raw)
+          )}
+        </div>
+        <div className='source'>
+          <p>
+            by: <strong>{props.data.contentfulBlogPost.source}</strong>
+          </p>
+        </div>
+        <div className='published'>
+          <p>
+            Published :{" "}
+            <small>{props.data.contentfulBlogPost.publishedDate}</small>
+          </p>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
+export default BlogPost;
